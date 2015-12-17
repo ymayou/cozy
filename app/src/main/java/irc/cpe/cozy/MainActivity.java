b9ee509a5fa2d76292571ff0530724e33e2c28a4
ymayou <mayou.yacine@gmail.com>
2015-12-17 16:57:30 +0100

    first commit

---
+++ app/src/main/java/irc/cpe/cozy/MainActivity.java
@@ -0,0 +1,13 @@
+package irc.cpe.cozy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
