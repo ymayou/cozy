a7da6f4af6ed160ebfe6e31ede5c89d03c7f7642
peluol <louis_lep@hotmail.fr>
2015-12-18 10:39:33 +0100

    ManifestIssue

--- app/src/main/java/irc/cpe/cozy/NavActivity.java
+++ app/src/main/java/irc/cpe/cozy/NavActivity.java
@@ -1,9 +1,8 @@
 package irc.cpe.cozy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class NavActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,8 +27,8 @@ public class NavActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                NavActivity.this.startActivity(new Intent(NavActivity.this, NewMarkActivity.class));
+                finish();
             }
         });
 
