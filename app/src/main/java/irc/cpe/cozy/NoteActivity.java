00cc70e44b1426031353b57ac07fe3295bfc02b5
peluol <louis_lep@hotmail.fr>
2016-01-04 17:05:45 +0100

    NoteActivity AlertDialog et entrée nouvelle note

--- app/src/main/java/irc/cpe/cozy/NoteActivity.java
+++ app/src/main/java/irc/cpe/cozy/NoteActivity.java
@@ -1,11 +1,16 @@
 package irc.cpe.cozy;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -23,15 +28,47 @@ public class NoteActivity extends AppCompatActivity {
                 NoteActivity.this.startActivity(new Intent(NoteActivity.this, NavActivity.class));
             }
         });
-/*
-        Intent current = getIntent();
-        String note = current.getStringExtra("NOTE");
-        if (note != null && !note.equals(""))
-        {
-            TextView content = (TextView) findViewById(R.id.textView2);
-            content.setText(note);
-        }
-*/
+
+        Button noteButton=(Button)findViewById(R.id.noteButton);
+
+        noteButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                final String title;
+                final String content;
+
+                EditText textTitle=(EditText)findViewById(R.id.noteTitle);
+                EditText textContent=(EditText)findViewById(R.id.noteContent);
+
+                title = textTitle.getText().toString();
+                content = textContent.getText().toString();
+
+                new AlertDialog.Builder(NoteActivity.this)
+                        .setTitle("Entrée nouvelle note")
+                        .setMessage("Voulez-vous enregistrer votre nouvelle note ?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Récupérer id du folder dans lequel est entrée la note
+                                //Note newNote = new Note(title, content, 1);
+                                Toast.makeText(getApplicationContext(), "Note '"+ title + "' sauvegardée",
+                                        Toast.LENGTH_LONG).show();
+                                finish();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(), "Note non sauvegardée",
+                                        Toast.LENGTH_LONG).show();
+                                NoteActivity.this.startActivity(new Intent(NoteActivity.this, NoteActivity.class));
+                                finish();
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+
+        });
     }
 
 }
