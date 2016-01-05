4bacb6f0614580f0efbac294535ecf050668b031
peluol <louis_lep@hotmail.fr>
2016-01-05 09:13:52 +0100

    EditNoteActivity Creation

--- app/src/main/java/irc/cpe/cozy/NoteActivity.java
+++ app/src/main/java/irc/cpe/cozy/NoteActivity.java
@@ -1,7 +1,5 @@
 package irc.cpe.cozy;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import irc.cpe.cozy.Rest.EditNoteActivity;
 import irc.cpe.cozy.Rest.WebserviceActivity;
 
 public class NoteActivity extends AppCompatActivity {
@@ -27,7 +26,8 @@ public class NoteActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NoteActivity.this.startActivity(new Intent(NoteActivity.this, NavActivity.class));
+                NoteActivity.this.startActivity(new Intent(NoteActivity.this, EditNoteActivity.class));
+                finish();
             }
         });
 
@@ -37,8 +37,8 @@ public class NoteActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View arg0) {
-                final String title;
-                final String content;
+                String title;
+                String content;
 
                 EditText textTitle=(EditText)findViewById(R.id.noteTitle);
                 EditText textContent=(EditText)findViewById(R.id.noteContent);
@@ -46,16 +46,19 @@ public class NoteActivity extends AppCompatActivity {
                 title = textTitle.getText().toString();
                 content = textContent.getText().toString();
 
+                // Récupérer id du folder dans lequel est entrée la note
+                //Note newNote = new Note(title, content, 1);
+                Toast.makeText(getApplicationContext(), "Note '"+ title + "' sauvegardée",
+                        Toast.LENGTH_LONG).show();
+                finish();
+
+                /*
                 new AlertDialog.Builder(NoteActivity.this)
                         .setTitle("Entrée nouvelle note")
                         .setMessage("Voulez-vous enregistrer votre nouvelle note ?")
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
-                                // Récupérer id du folder dans lequel est entrée la note
-                                //Note newNote = new Note(title, content, 1);
-                                Toast.makeText(getApplicationContext(), "Note '"+ title + "' sauvegardée",
-                                        Toast.LENGTH_LONG).show();
-                                finish();
+                            //Action après validation
                             }
                         })
                         .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
@@ -68,12 +71,29 @@ public class NoteActivity extends AppCompatActivity {
                         })
                         .setIcon(android.R.drawable.ic_dialog_alert)
                         .show();
+                        */
+            }
+
+        });
+
+        Button noteCancel=(Button)findViewById(R.id.noteCancel);
+
+        noteCancel.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(getApplicationContext(), "Note non sauvegardée",
+                        Toast.LENGTH_LONG).show();
+                NoteActivity.this.startActivity(new Intent(NoteActivity.this, NoteActivity.class));
+                finish();
+
             }
 
         });
 
         Intent newIntent = new Intent(getApplicationContext(), WebserviceActivity.class);
-        startActivity(newIntent);
+        //startActivity(newIntent);
     }
 
 }
--- app/src/main/java/irc/cpe/cozy/Rest/EditNoteActivity.java
+++ app/src/main/java/irc/cpe/cozy/Rest/EditNoteActivity.java
@@ -1,11 +1,15 @@
 package irc.cpe.cozy.Rest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import irc.cpe.cozy.NavActivity;
 import irc.cpe.cozy.R;
 
 public class EditNoteActivity extends AppCompatActivity {
@@ -17,13 +21,45 @@ public class EditNoteActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                EditNoteActivity.this.startActivity(new Intent(EditNoteActivity.this, NavActivity.class));
+                finish();
             }
+        });
+
+        Button editOk=(Button)findViewById(R.id.editOk);
+
+        editOk.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(getApplicationContext(), "Modification de 'Title' sauvegardée",
+                        Toast.LENGTH_LONG).show();
+                EditNoteActivity.this.startActivity(new Intent(EditNoteActivity.this, NavActivity.class));
+                finish();
+
+            }
+
+        });
+
+        Button editCancel=(Button)findViewById(R.id.editCancel);
+
+        editCancel.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(getApplicationContext(), "Modification de 'Title' non sauvegardée",
+                        Toast.LENGTH_LONG).show();
+                EditNoteActivity.this.startActivity(new Intent(EditNoteActivity.this, NavActivity.class));
+                finish();
+
+            }
+
         });
     }
 
