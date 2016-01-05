e81e9fc3c9e39d35307efad5ab238b6adcd8bab4
peluol <louis_lep@hotmail.fr>
2016-01-05 15:37:45 +0100

    Tests dans ExplorerFragment

--- app/src/main/java/irc/cpe/cozy/ExplorerFragment.java
+++ app/src/main/java/irc/cpe/cozy/ExplorerFragment.java
@@ -3,7 +3,6 @@ package irc.cpe.cozy;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
