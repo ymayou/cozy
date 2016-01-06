36ac43ecb04b0e4f458fedb8dde65b570a742135
peluol <louis_lep@hotmail.fr>
2016-01-06 11:32:36 +0100

    NoteActivity Update & Delete

--- app/src/main/java/irc/cpe/cozy/NoteActivity.java
+++ app/src/main/java/irc/cpe/cozy/NoteActivity.java
@@ -1,6 +1,5 @@
 package irc.cpe.cozy;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,9 +10,8 @@ import android.widget.Toast;
 
 import irc.cpe.cozy.Dao.NoteDao;
 import irc.cpe.cozy.Model.Note;
-import irc.cpe.cozy.Rest.WebserviceActivity;
 
-public class NoteActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,63 +19,59 @@ public class NoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_note);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //RadioButton bold = (RadioButton)findViewById(R.id.noteBold);
         //Récupération de l'id de la note à modifier
-
         int idNote=-1;
         Bundle extras = getIntent().getExtras();
-        idNote = extras.getInt("NOTE");
+        if (extras != null) {
+            idNote = extras.getInt("NOTE");
+        }
         /*
         String test = Integer.toString(extras.getInt("NOTE"));
         Toast.makeText(getApplicationContext(), "ID : '"+ test + "'",
                 Toast.LENGTH_LONG).show();*/
 
-
-        final EditText textTitle=(EditText)findViewById(R.id.noteTitle);
-        final EditText textContent=(EditText)findViewById(R.id.noteContent);
-
-
+        EditText textTitle=(EditText)findViewById(R.id.noteTitle);
+        EditText textContent=(EditText)findViewById(R.id.noteContent);
 
         if (idNote > -1){
             NoteDao noteDao = new NoteDao();
-            Note noteChanged = new Note();
-            noteChanged = noteDao.selectById(getApplicationContext(), idNote);
-
-            //Récupérer contenu note en base et insérer valeurs dans editText
+            //Note noteChanged = new Note();
+            //Récupération du contenu note en base et insertion valeurs dans editText
+            Note noteChanged = noteDao.selectById(getApplicationContext(), idNote);
             textTitle.setText(noteChanged.getName());
             textContent.setText(noteChanged.getContent());
-
+            Toast.makeText(getApplicationContext(), "Contenu de la note récupérée : '"+ noteChanged.getContent() +"'",
+                    Toast.LENGTH_LONG).show();
         }
 
         Button noteButton=(Button)findViewById(R.id.noteButton);
-
         final int finalId = idNote;
+        final String finalTitle = textTitle.getText().toString();
         noteButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View arg0) {
-                String title;
-                String content;
-
-                title = textTitle.getText().toString();
-                content = textContent.getText().toString();
-
+                EditText newTextTitle=(EditText)findViewById(R.id.noteTitle);
+                EditText newTextContent=(EditText)findViewById(R.id.noteContent);
+                String newTitle = newTextTitle.getText().toString();
+                String newContent = newTextContent.getText().toString();
                 if(finalId >-1){
                     //Modification de la note existante
                     NoteDao noteDao = new NoteDao();
-                    //noteDao.update(getApplicationContext(), newNote);
+                    Note newNote = new Note(finalId, newTitle, newContent, 0);
+                    noteDao.update(getApplicationContext(), newNote);
+                    Toast.makeText(getApplicationContext(), "Note '"+ newTitle + "' sauvegardée, Contenu : " + newContent,
+                            Toast.LENGTH_LONG).show();
+                    finish();
                 }else{
-                    Note newNote = new Note(title, content, 0);
+                    Note newNote = new Note(newTitle, newContent, 0);
                     //Insertion de la nouvelle note
                     NoteDao noteDao = new NoteDao();
                     noteDao.insert(getApplicationContext(), newNote);
+                    Toast.makeText(getApplicationContext(), "Note '"+ newTitle + "' sauvegardée, Contenu : " + newContent,
+                            Toast.LENGTH_LONG).show();
+                    finish();
                 }
-
-
-
-                Toast.makeText(getApplicationContext(), "Note '"+ title + "' sauvegardée",
-                        Toast.LENGTH_LONG).show();
-                finish();
-
                 /*
                 new AlertDialog.Builder(NoteActivity.this)
                         .setTitle("Entrée nouvelle note")
@@ -103,23 +97,28 @@ public class NoteActivity extends AppCompatActivity {
         });
 
         Button noteCancel=(Button)findViewById(R.id.noteCancel);
-
         noteCancel.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View arg0) {
-
                 Toast.makeText(getApplicationContext(), "Note non sauvegardée",
                         Toast.LENGTH_LONG).show();
-                NoteActivity.this.startActivity(new Intent(NoteActivity.this, NavActivity.class));
                 finish();
-
             }
-
         });
 
-        Intent newIntent = new Intent(getApplicationContext(), WebserviceActivity.class);
+        Button noteDelete=(Button)findViewById(R.id.noteDelete);
+        noteDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                NoteDao noteDao = new NoteDao();
+                noteDao.delete(getApplicationContext(), finalId);
+                Toast.makeText(getApplicationContext(), "Note "+finalTitle+" supprimée",
+                        Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        //Intent newIntent = new Intent(getApplicationContext(), WebserviceActivity.class);
         //startActivity(newIntent);
     }
-
 }
