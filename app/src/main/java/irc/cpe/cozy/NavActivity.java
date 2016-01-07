47d2b7556c04c5eb58fe7237d7f5dbef67855921
theosdoron <teddy.gustiaux@gmail.com>
2016-01-07 14:56:09 +0100

    Ajout du Service

--- app/src/main/java/irc/cpe/cozy/NavActivity.java
+++ app/src/main/java/irc/cpe/cozy/NavActivity.java
@@ -1,7 +1,11 @@
 package irc.cpe.cozy;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +19,7 @@ import android.view.SubMenu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +31,7 @@ import irc.cpe.cozy.Dao.FolderDao;
 import irc.cpe.cozy.Dao.NoteDao;
 import irc.cpe.cozy.Model.Explorer;
 import irc.cpe.cozy.Model.Folder;
+import irc.cpe.cozy.Rest.LocalService;
 
 public class NavActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
@@ -106,6 +112,7 @@ public class NavActivity extends AppCompatActivity
             }
         });
 
+        doBindService();
     }
 
     @Override
@@ -266,6 +273,61 @@ public class NavActivity extends AppCompatActivity
                 return onNavigationItemSelected(item);
             }
         });
+    }
+
+
+    /** Flag indicating whether we have called bind on the service. */
+    boolean mIsBound;
+
+    private LocalService mBoundService;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the service object we can use to
+            // interact with the service.  Because we have bound to a explicit
+            // service that we know is running in our own process, we can
+            // cast its IBinder to a concrete class and directly access it.
+            mBoundService = ((LocalService.LocalBinder)service).getService();
+
+            // Tell the user about this for our demo.
+            Toast.makeText(NavActivity.this, R.string.local_service_connected,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            // Because it is running in our same process, we should never
+            // see this happen.
+            mBoundService = null;
+            Toast.makeText(NavActivity.this, R.string.local_service_disconnected,
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    void doBindService() {
+        // Establish a connection with the service.  We use an explicit
+        // class name because we want a specific service implementation that
+        // we know will be running in our own process (and thus won't be
+        // supporting component replacement by other applications).
+        bindService(new Intent(NavActivity.this,
+                LocalService.class), mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        doUnbindService();
     }
 
 }
---
+++ app/src/main/java/irc/cpe/cozy/Rest/LocalServiceActivities.java
@@ -0,0 +1,23 @@
+package irc.cpe.cozy.Rest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class LocalServiceActivities {
+    /**
+     * <p>Example of explicitly starting and stopping the local service.
+     * This demonstrates the implementation of a service that runs in the same
+     * process as the rest of the application, which is explicitly started and stopped
+     * as desired.</p>
+     * <p/>
+     * <p>Note that this is implemented as an inner class only keep the sample
+     * all together; typically this code would appear in some separate class.
+     */
+    public static class Controller extends Activity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+    }
+}
